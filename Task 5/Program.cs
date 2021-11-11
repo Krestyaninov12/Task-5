13e87a5868f246ee3c1e9ec0b5fcf5b7d103ba12
Krestyaninov12 <kreda@pnpe.ru>
2021-11-11 22:52:18 +0300

    Добавьте файлы проекта.

---
+++ Task 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int i;
+            int aSum;
+            aSum = 0;
+            int[] array = new int[7];
+            for (i = 0; i < 7; i++)
+            {
+                Console.Write("Введите элемент массива под индексом {0}:\t", i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Вы ввели следующий массив");
+            for (int j = 0; j < 7; j++)
+            { Console.WriteLine(array[j]); }
+
+            for (int k = 0; k < 7; k++)
+            { aSum = aSum + array[k]; }
+            Console.WriteLine("Среднеарифметическое составляет {0}", (aSum / 7)); 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 5_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, k, n;
+            k = 0;
+            
+            Console.WriteLine("Введите размерность массива");
+            n = int.Parse(Console.ReadLine());
+            int[,] array = new int[n, n];
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (j = 0; j < n; j++)
+                {
+                 
+                    if (i % 2 != 1)
+                    {
+                        if (j % 2 != 1)
+                        {
+                            array[i, j] = 1;
+                        }                                               
+                    }
+                    if (i % 2 == 1)
+                    {
+                        if (j % 2 == 1)
+                        {
+                            array[i, j] = 1;
+                        }
+                    }
+                                             
+                 Console.Write(String.Format("{0,3}", array[i, j]));
+                }
+                Console.WriteLine();
+            } Console.ReadKey();
+                   
+
+        }
+    }
+}
